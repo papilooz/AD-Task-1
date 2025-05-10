17712d489b62d752ad8b0c7501ce2e624365b5a4
papilooz <manriquequervie@gmail.com>
2025-05-10 18:22:32 +0800

    refactor(landing page): enhance

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header with Two Navs</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
+    <link rel="stylesheet" href="assets/css/style.css"></head>
 <body>
     <header>
         <div class="container">
