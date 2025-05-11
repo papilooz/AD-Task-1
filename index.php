b3885614c42ddba51350f1b321b4c3ac9d8a356c
papilooz <manriquequervie@gmail.com>
2025-05-11 20:12:21 +0800

    feat(index): add image

--- index.php
+++ index.php
@@ -7,24 +7,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header with Two Navs</title>
-    <link rel="stylesheet" href="assets/css/style.css"></head>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
 <body>
     <header>
         <div class="container">
-
-            <!-- First navigation -->
             <nav class="nav1">
                 <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="about.php">About</a></li>
+                    <li><a href="index.php">About Me</a></li>
+                    <li><a href="about.php">PHP</a></li>
                 </ul>
             </nav>
         </div>
     </header>
 
-    <!-- Background Section -->
     <section class="background-section">
-
     </section>
+     <div class="packages-container">
+            <h2>QUERVIE MANRIQUE</h2>
+            <hr style="border-top: 1px solid #ccc; width: 50%; margin: 10px auto;">
+            <div class="packages-grid">
+                <div class="package-item">
+                    <img src="assets/img/digital-printing-icon.png" alt="Digital Printing">
+                </div>
+                
+               
+            </div>
+        </div>
+          <section class="repair-section">
+        <div class="container">
+            <div class="left-content">
+                <div class="trusted-box">
+                    <i class="fas fa-users"></i>
+                    <h2>Football</h2>
+
+                </div>
+                <img src="assets/img/quervie.jpg" alt="Mechanic " class="main-image">
+                <div class="experience-box">
+                    <i class="fas fa-trophy"></i>
+                    <h2>Volleyball</h2>
+                </div>
+            </div>
+            <div class="right-content">
+                <h1>I love my bebe rose ann </h1>
+                <p>test</p>
+                <div class="service-box">
+                    <i class="fas fa-car"></i>
+                    <div>
+                        <h4>test</h4>
+                        <p>test</p>
+                    </div>
+                </div>
+                <div class="service-box">
+                    <i class="fas fa-hard-hat"></i>
+                    <div>
+                        <h4>test</h4>
+                        <p>test</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
 </body>
 </html>
