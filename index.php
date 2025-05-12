7b4ce2f2504ec02b44d65a7f85f23ec15e019969
papilooz <manriquequervie@gmail.com>
2025-05-12 19:35:13 +0800

    refactor(index): add new html elements

--- index.php
+++ index.php
@@ -9,15 +9,18 @@
     <title>AD-Task-1</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+      <link rel="icon" href="/assets/img/qwe.png" type="image/x-icon">
+
 
 </head>
 <body>
     <header>
         <div class="container">
+    
             <nav class="nav1">
                 <ul>
                     <li><a href="index.php">About Me</a></li>
-                    <li><a href="../page/Requirements.php">Requirements</a></li>
+                    <li><a href="../page/Requirements.php">AD-Task-1</a></li>
                 </ul>
             </nav>
         </div>
@@ -26,23 +29,22 @@
     <section class="background-section">
       
      <div class="package-selection">
-        <h2>CHOOSE FROM A VARIETY</h2>
-        <h2>OF PACKAGES.</h2>
+        <h2>AD-Task-1</h2>
         <div class="separator"></div>
         <div class="event-types icon-nav-container">
             <div class="event-item icon-nav-item">
-                <a href="DigitalPrint.html">
+                <a href="../page/Requirements.php">
                     <div class="icon-circle">
-                        <img src="./images/print.png" alt="Digital Printing">
+                        <img src="/assets/img/declar.png" alt="">
                     </div>
                     <h3>Declarations</h3>
                     <span class="know-more">Show</span>
                 </a>
             </div>
             <div class="event-item icon-nav-item">
-                <a href="binding.html">
+                <a href="../page/Requirements.php">
                     <div class="icon-circle">
-                        <img src="./images/binding.png" alt="Binding">
+                        <img src="/assets/img/declar.png" alt="">
                     </div>
                     <h3>Conditional</h3>
                                        <span class="know-more">Show</span>
@@ -50,11 +52,11 @@
                 </a>
             </div>
              <div class="event-item icon-nav-item">
-                <a href="binding.html">
+                <a href="../page/Requirements.php">
                     <div class="icon-circle">
-                        <img src="./images/binding.png" alt="Binding">
+                        <img src="/assets/img/declar.png" alt="">
                     </div>
-                    <h3>looping</h3>
+                    <h3>Looping</h3>
                  <span class="know-more">Show</span>
 
                 </a>
@@ -72,7 +74,7 @@
 
 
                 </div>
-                <img src="assets/img/quervie.jpg" alt="Mechanic " class="main-image">
+                <img src="assets/img/quervie.jpg" alt=" " class="main-image">
                 <div class="experience-box">
                     <i class="fas fa-trophy"></i>
 
@@ -81,7 +83,7 @@
             <div class="right-content">
                 <h1>About Me</h1>
                 <div class="service-box">
-                    <i class="fas fa-car"></i>
+<i class="fas fa-fire"></i>
                     <div>
                       
                         <p>I’m an athletic person with a deep passion for movement and expression. I love playing a wide variety of sports whether it’s on the field, the court, or the track, I feel most alive when I’m active. Alongside sports, dancing is a huge part of who I am. I enjoy all genres of dance, from hip-hop and salsa to ballet and swing. I’m competing in sports or flowing to the beat, I’m always chasing growth, joy, and the thrill of movement.</p>
@@ -90,7 +92,7 @@
                 <div class="service-box">
                     <i class="fas fa-hard-hat"></i>
                     <div>
-                        <p>My dream is to become a skilled and creative web designer someday and pursue the things I truly love in the future. I want to bring ideas to life through design, create meaningful user experiences, and constantly grow in a field that combines technology and creativity. It's not just a career goal—it’s a passion I’m excited to turn into reality, one project at a time.</p>
+                        <p>My dream is to become a skilled and creative web designer someday and pursue the things I truly love in the future. I want to bring ideas to life through design, create meaningful user experiences, and constantly grow in a field that combines technology and creativity. It's not just a career goal it’s a passion I’m excited to turn into reality, one project at a time.</p>
                     </div>
                 </div>
             </div>
@@ -101,13 +103,13 @@
     <div class="footer-container">
         <p>&copy; 2025 Quervie Manrique. All rights reserved.</p>
         <p>Follow me on 
-            <a href="https://facebook.com" target="_blank">
+            <a href="https://www.facebook.com/kervimanrique/" target="_blank">
                 <img src="assets/img/facebook.png" alt="Facebook" class="social-icon">
             </a> 
-            <a href="https://instagram.com" target="_blank">
+            <a href="https://www.instagram.com/qrvi146/" target="_blank">
                 <img src="assets/img/instagram.png" alt="Instagram" class="social-icon">
             </a> 
-            <a href="https://linkedin.com" target="_blank">
+            <a href="https://ph.linkedin.com/in/quervie-manrique-9863a928b" target="_blank">
                 <img src="assets/img/linkedin.png" alt="LinkedIn" class="social-icon">
             </a>
         </p>
